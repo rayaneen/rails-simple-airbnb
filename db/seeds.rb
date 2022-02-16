d0f9045753a79c71a64711edd8a6aa80a32d09cf
Rayane Nordine <rayanenordine@gmail.com>
2022-02-16 16:30:39 +0100

    Added index and a bit of css -- unfinished: to be continued

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,18 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+puts "Cleaning..."
+Flat.destroy_all
+puts "DB cleaned!"
+puts "Creating Flats"
+5.times do
+  flat = Flat.create(
+    name: Faker::Space.moon,
+    address: Faker::Space.constellation,
+    description: Faker::Space.galaxy,
+    price_per_night: rand(100..200),
+    number_of_guests: rand(1..10)
+  )
+  puts "#{flat.name} - adress: #{flat.address} - description #{flat.description} created"
+end
+
+puts "Done!"
